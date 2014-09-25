1b883e750f32886197a28238f096acbe4f71ba08
EnamiBatte <Akouriat@andrew.cmu.edu>
2014-09-25 17:17:34 -0400

    RMI
    RMI Initial Files

---
+++ RMI/src/NameServer.java
@@ -0,0 +1,6 @@
+public interface NameServer // extends YourRemote 
+{
+    public RemoteObjectRef match(String name);
+    public NameServer add(String s, RemoteObjectRef r, NameServer n);
+    public NameServer next();   
+}
