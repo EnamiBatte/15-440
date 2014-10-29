f5f4cb69c4d7390075f3e8150d63c9513011d1a5
EnamiBatte <Akouriat@andrew.cmu.edu>
2014-10-29 18:54:59 -0400

    MapReduce
    Start of MapReduce

---
+++ MapReduce/src/Configuration.java
@@ -0,0 +1,11 @@
+
+public class Configuration {
+	public static final String Master_Address = "unix1.andrew.cmu.edu";
+	public static final String[] Slave_Addresses = {"unix2.andrew.cmu.edu","unix3.andrew.cmu.edu"};
+	public static final int port = 4451;
+	public static final int timeout = 500;
+	//Need size of each map
+	//Number of maps/Per Host
+	//Number of Reduces/Per Host
+	//Other constants as needed
+}
---
+++ MapReduce/src/MasterController.java
@@ -0,0 +1,9 @@
+
+public class MasterController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
