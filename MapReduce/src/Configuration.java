9f324d11738fc00c3b74ac57b6d4822c36dd1d22
stcat <zhangshujie27@gmail.com>
2014-11-21 13:32:22 -0500

    Update Configuration.java

--- MapReduce/src/Configuration.java
+++ MapReduce/src/Configuration.java
@@ -8,6 +8,8 @@ public class Configuration {
 	public static final int length = 1000;
 	//Need size of each map
 	//Unix3 has 6 cores
+	public static final int numberOfLines = 5;
+	public static final int numberOfReducers = 2;
 	public static final int maxMapsPerHost = 6;
 	public static final int mapPartitionSize = 1;
 	//public static final int mapRetries = 2;
