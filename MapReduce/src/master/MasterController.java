fe44055badde76e35cb5426225c7b9d4f9b0d694
EnamiBatte <Akouriat@andrew.cmu.edu>
2014-11-21 22:12:17 -0500

    Fixing the Master

--- MapReduce/src/master/MasterController.java
+++ MapReduce/src/master/MasterController.java
@@ -1,63 +1,75 @@
 package master;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class MasterController {
-	//Start System
-	//Stop System
-	//Kill Job
+	public static boolean running = true;
+	public static BufferedReader reader;
 	
-	//Output Following
-	//Queue Jobs
-	//Running Jobs
-	//Where Job
-	
-	
-	//Check for Timeout
-	//Attempt to Restart Communication
-	//Send to Other Participant
-	
+	public static void stopMaster()
+	{
+		running = false;
+		try {
+			reader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
 	
 	
 	public static void main(String[] args) {
 		MasterCoordinator mc = new MasterCoordinator();
+		reader = new BufferedReader(new InputStreamReader(System.in));
 		
-		// TODO Auto-generated method stub
-		String arg = args[0];
-		if(arg.equals("print"))
-		{
-			String arg2= args[1];
-			if(arg2.equals("queue"))
-			{
-				mc.printQueue();
-			}
-			else if(arg2.equals("finished"))
-			{
-				mc.printFinished();
-			}
-			else if(arg2.equals("running"))
-			{
-				mc.printRunning();
-			}
-			else{
-				System.out.println("Unrecognized Print Command");
+		while (running) {
+			try {
+				String input = reader.readLine();
+				String[] jobArgs = input.split(" ");
+				String arg = jobArgs[0];
+				if(arg.equals("print"))
+				{
+					String arg2= jobArgs[1];
+					if(arg2.equals("queue"))
+					{
+						mc.printQueue();
+					}
+					else if(arg2.equals("finished"))
+					{
+						mc.printFinished();
+					}
+					else if(arg2.equals("running"))
+					{
+						mc.printRunning();
+					}
+					else{
+						System.out.println("Unrecognized Print Command");
+					}
+				}
+				else if(arg.equals("kill"))
+				{
+					//Error processing
+					try{
+					int arg2= Integer.parseInt(jobArgs[1]);
+					mc.kill(arg2);
+					}
+					catch (NumberFormatException e)
+					{
+						System.out.println("Kill not called on a valid job.");
+					}
+				}
+				else{
+					
+				}
+			}catch(IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
-		else if(arg.equals("kill"))
-		{
-			//Error processing
-			try{
-			int arg2= Integer.parseInt(args[1]);
-			mc.kill(arg2);
-			}
-			catch (NumberFormatException e)
-			{
-				System.out.println("Kill not called on a valid job.");
-			}
-		}
-		else{
-			
-		}
-		
 	}
 
 }
