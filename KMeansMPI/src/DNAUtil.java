865a68b2a330cdce30b104d44e42ec00bdc347bb
stcat <zhangshujie27@gmail.com>
2014-12-02 20:20:37 -0500

    Update DNAUtil.java

--- KMeansMPI/src/DNAUtil.java
+++ KMeansMPI/src/DNAUtil.java
@@ -25,10 +25,49 @@ public class DNAUtil extends ClusterUtil {
 
 	@Override
 	public Datapoint getCentroid(List<Datapoint> cluster) {
-		String res = "";
-		List<String> resString = new LinkedList<String>();
-		//Need Clean way to get Centroid
-		return null;
+		int length = cluster.get(0).size();
+		int[][] dna = new int[4][length];
+		String[] ret = new String[length];
+		int[] max = new int[length];
+		for (Datapoint dp : cluster) {
+			List<String> str = dp.getValue();
+			for (int i = 0; i < str.size(); i++) {
+				switch (str.get(i).charAt(0)) {
+				case 'A':
+					dna[0][i]++;
+					break;
+				case 'T':
+					dna[1][i]++;
+					break;
+				case 'G':
+					dna[2][i]++;
+					break;
+				case 'C':
+					dna[3][i]++;
+				}
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < length; j++) {
+				if (max[j] < dna[i][j]) {
+					max[j] = dna[i][j];
+					switch (i) {
+					case '0':
+						ret[j] = "A";
+						break;
+					case '1':
+						ret[j] = "T";
+						break;
+					case '2':
+						ret[j] = "G";
+						break;
+					case '3':
+						ret[j] = "C";
+					}
+				}
+			}
+		}
+		return Arrays.asList(ret);
 	}
 
 }
