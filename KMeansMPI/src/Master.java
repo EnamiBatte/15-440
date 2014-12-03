5e5ff63ee080608848e5ebca1910a7a8205f71ff
EnamiBatte <Akouriat@andrew.cmu.edu>
2014-12-03 18:32:51 -0500

    Some Slave Work

--- KMeansMPI/src/Master.java
+++ KMeansMPI/src/Master.java
@@ -1,4 +1,25 @@
+import java.util.List;
+
 
 public class Master {
+	
+	public static List<Datapoint> doKMeans(List<Datapoint> input, List<Datapoint> centriods, ClusterUtil cd)
+	{
+		
+		
+		return centriods;
+	}
+	
+	public static boolean endPoint(List<Integer> as, List<Integer> bs) {
+		int maxDifference = 0;
+		int ret = 0;
+		if (as.size() != bs.size()) {
+			return false;
+		}
+		for (int i = 0; i < as.size(); i++) {
+			ret += as.get(i) ^ bs.get(i);
+		}
+		return (ret <= maxDifference);
+	}
 
 }
--- KMeansMPI/src/Slave.java
+++ KMeansMPI/src/Slave.java
@@ -1,10 +1,36 @@
-import mpi.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class Slave {
 	
-	public void associate()
-	{
-		
+	public static List<Integer> assign(List<Datapoint> points, List<Datapoint> centriods, ClusterUtil cd) {
+		List<Integer> ret = new ArrayList<Integer>();
+		for (Datapoint p : points) {
+			double distance = cd.getDistance(p, centriods.get(0));
+			int index = 0;
+			for (int i = 1; i < centriods.size(); i++) {
+				if (distance > cd.getDistance(p, centriods.get(i))) {					
+					index = i;
+				}
+			}
+			ret.add(index);
+		}
+		return ret;
 	}
+	
+	public static List<Datapoint> doKMeans(List<List <Datapoint>> input, ClusterUtil cd) {
+		List<Datapoint> centriods = new LinkedList<Datapoint>();
+		for (List<Datapoint> cluster : input)
+		{
+			centriods.add(cd.getCentroid(cluster));
+		}
+		return centriods;
+	}
+	
+	
+	
+	
 
 }
