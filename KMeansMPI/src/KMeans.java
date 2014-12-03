67eb65d525abe82f6e92a6b3e6dabc874b021d93
stcat <zhangshujie27@gmail.com>
2014-12-03 11:17:56 -0500

    Update KMeans.java

--- KMeansMPI/src/KMeans.java
+++ KMeansMPI/src/KMeans.java
@@ -1,13 +1,15 @@
 import java.util.*;
 
+
+
 public class KMeans {
 	public static void main(String[] args) {
 		List<Datapoint> input = new LinkedList<Datapoint>();
 		List<Datapoint> centriods = new LinkedList<Datapoint>();
-		int numOfInput = 10;
-		int numOfCluster = 2;
-		CoordUtil cd = new CoordUtil();
+		ClusterUtil cd;
+
 		
+		cd = new CoordUtil();
 		input.add(new Coordinate(1.1, 2.1));
 		input.add(new Coordinate(1.2, 2.2));
 		input.add(new Coordinate(1.3, 2.3));
@@ -20,11 +22,38 @@ public class KMeans {
 		input.add(new Coordinate(11.0, -1.9));
 		centriods.add(new Coordinate(0, 0));
 		centriods.add(new Coordinate(5, 5));
+		/*
+		cd = new DNAUtil();
+		input.add(new DNAStrand("AAAAAA"));
+		input.add(new DNAStrand("ATTAAA"));
+		input.add(new DNAStrand("AAAGAA"));
+		input.add(new DNAStrand("CAAAAA"));
+		input.add(new DNAStrand("GAGCGG"));
+		input.add(new DNAStrand("GGTCGG"));
+		input.add(new DNAStrand("GATCGG"));
+		input.add(new DNAStrand("GATAGG"));
+		input.add(new DNAStrand("GATCCG"));
+		input.add(new DNAStrand("CCAATA"));
+		input.add(new DNAStrand("CCAATT"));
+		input.add(new DNAStrand("CCAATG"));
+		centriods.add(new DNAStrand("AAAAAA"));
+		centriods.add(new DNAStrand("CCCATC"));
+		centriods.add(new DNAStrand("GGGGGG"));
+		*/
 		
-		List<Integer> as = assign(input, centriods);
+		
+		List<Datapoint> output = doKMeans(input, centriods, cd);
+		for (Datapoint d : output) {
+			System.out.println(d.getValue());
+		}
+	}
+	
+	public static List<Datapoint> doKMeans(List<Datapoint> input, List<Datapoint> centriods, ClusterUtil cd) {
+		List<Integer> as = assign(input, centriods, cd);
 		List<Integer> bs = new LinkedList<Integer>();
-
-		while (!as.equals(bs)) {
+		int numOfCluster = centriods.size();
+		
+		while (!endPoint(as, bs)) {
 			bs = as;
 			centriods = new LinkedList<Datapoint>();
 			for (int i = 0; i < numOfCluster; i++) {
@@ -33,20 +62,30 @@ public class KMeans {
 					if (i == as.get(j)) {
 						tmp.add(input.get(j));
 					}
-				}				
+				}
 				centriods.add(cd.getCentroid(tmp));
 			}
-			as = assign(input, centriods);	
+			
+			as = assign(input, centriods, cd);	
 		}
 		
-		for (int i = 0; i < numOfInput; i++) {
-			System.out.println(bs.get(i));
+		return centriods;
+		
+	}
+	public static boolean endPoint(List<Integer> as, List<Integer> bs) {
+		int maxDifference = 0;
+		int ret = 0;
+		if (as.size() != bs.size()) {
+			return false;
 		}
+		for (int i = 0; i < as.size(); i++) {
+			ret += as.get(i) ^ bs.get(i);
+		}
+		return (ret <= maxDifference);
 	}
 	
-	public static List<Integer> assign(List<Datapoint> points, List<Datapoint> centriods) {
+	public static List<Integer> assign(List<Datapoint> points, List<Datapoint> centriods, ClusterUtil cd) {
 		List<Integer> ret = new ArrayList<Integer>();
-		CoordUtil cd = new CoordUtil();
 		for (Datapoint p : points) {
 			double distance = cd.getDistance(p, centriods.get(0));
 			int index = 0;
