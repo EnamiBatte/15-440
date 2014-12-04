4cf4ec45fba84039cd30f895bc277b34e93c5160
EnamiBatte <Akouriat@andrew.cmu.edu>
2014-12-03 21:13:41 -0500

    Start of Master

--- KMeansMPI/src/Main.java
+++ KMeansMPI/src/Main.java
@@ -1,10 +1,21 @@
+//import mpi.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		//MPI.Init(args);
 		
+		//int rank;
+	//	rank = MPI.COMM_WORLD.Rank();
+		//if(rank == 0)
+	//	{
+		//	Master.run(args);
+		//}
+		//else{
+			Slave.run();
+		//}
 		
-		
+		//MPI.Finalize();
 	}
 
 }
--- KMeansMPI/src/Master.java
+++ KMeansMPI/src/Master.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 
@@ -20,6 +24,27 @@ public class Master {
 			ret += as.get(i) ^ bs.get(i);
 		}
 		return (ret <= maxDifference);
+	}
+
+	public static void run(String[] args) throws Exception {
+		if(args.length < 1)
+		{
+			System.out.println("Help");
+			return;
+		}
+		else{
+			//args[0] = dna or coord
+			//args[1] = seq or distr
+			FileInputStream input = new FileInputStream(args[2]);
+			BufferedReader read = new BufferedReader(new InputStreamReader(input));
+			int numClusters = Integer.parseInt(args[3]);
+			//Assignments
+			
+			
+			
+		}	
+		
+		
 	}
 
 }
